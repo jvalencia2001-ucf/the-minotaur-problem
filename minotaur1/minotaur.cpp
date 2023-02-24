9a7c363bdc024a43df6c165dbdc0031e0e033cec
jvalencia2001-ucf <jvalencia.ucf@gmail.com>
2023-02-23 22:02:19 -0500

    Added comments.

--- minotaur1/minotaur.cpp
+++ minotaur1/minotaur.cpp
@@ -5,12 +5,14 @@
 #include <vector>
 using namespace std;
 
+//Create necessary variables
 int finished = 0;
 int active_guest = -1;
 int is_cupcake = 1;
 int epochs = 0;
 std::mutex m;
 
+//Function to generate a random number to represent guest chosen.
 int generate_random(int range_from, int range_to) {
     std::random_device                  rand_dev;
     std::mt19937                        generator(rand_dev());
@@ -19,8 +21,10 @@ int generate_random(int range_from, int range_to) {
     return distr(generator);
 }
 
+//Function for the thread representing the minotaur.
 void minotaur() {
     while(finished == 0) {
+        //Chooose a new guest and increase the epochs.
         m.lock();
         if(active_guest == -1) {
             int guest_chosen = generate_random(1, 10);
@@ -32,18 +36,23 @@ void minotaur() {
     }
 }
 
+//Function for the thread representing the counter thread.
 void counter_thread(int thread_num) {
     int counter = 0;
     while (finished == 0) {
+        //Check if the current thread represents the guest chosen.
         if(active_guest == thread_num) {
-            cout << "counter active\n";
+            cout << "Counter active\n";
+            //If there is a cupcake in the room, eat it and increase the count.
             if(is_cupcake == 1){
                 is_cupcake = 0;
                 counter++;
-                cout << "counter incremented to: " << counter << '\n';
+                cout << "Counter incremented to: " << counter << '\n';
+                //Finish all threads if counter reaches 10.
                 if(counter == 10)
                     finished = 1;
             }
+            //Reset the active guest.
             m.lock();
             active_guest = -1;
             m.unlock();
@@ -52,15 +61,19 @@ void counter_thread(int thread_num) {
     cout << "Final counter: " << counter << '\n';
 }
 
+//Function for the threads representing drone guests.
 void drone_thread(int thread_num) {
     int participated = 0;
     while(finished == 0) {
+        //Check if the current thread represents the guest chosen.
         if(active_guest == thread_num) {
             cout << "drone active\n";
+            //If there is no cupcake in the room, request one and record participation.
             if(is_cupcake == 0  && participated == 0) {
                 is_cupcake = 1;
                 participated = 1;
             }
+            //Reset the active guest.
             m.lock();
             active_guest = -1;
             m.unlock();
@@ -70,6 +83,7 @@ void drone_thread(int thread_num) {
 
 int main() {
 
+    //Create and join all threads.
     std::thread t1(minotaur);
     
     std::thread t2(counter_thread, 1);
